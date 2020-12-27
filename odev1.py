064ec3c70d0371d63861f4113c675af1a95f7d12
Abidin ARPACI <abidinarpaci@gmail.com>
2020-12-27 21:38:41 +0300

    Odev1.py
    1. Ödev Abidin ARPACI abidinarpaci@gmail.com

---
+++ odev1.py
@@ -0,0 +1,35 @@
+#Ödev 1
+
+#Hazırlayan: Abidin ARPACI
+#İletişim: abidinarpaci@gmail.com
+
+deger1=input("1. Değeri Giriniz:")
+
+deger2=input("2. Değeri Giriniz:")
+
+deger3=input("3. Değeri Giriniz:")
+
+deger4=input("4. Değeri Giriniz:")
+
+deger5=input("5. Değeri Giriniz:")
+
+print("Girilen Değerler ve Tipleri")
+
+print("1. Girilen Değer {} ve Veri Tipi{}".format(deger1,type(deger1)))
+print("2. Girilen Değer {} ve Veri Tipi{}".format(deger2,type(deger2)))
+print("3. Girilen Değer {} ve Veri Tipi{}".format(deger3,type(deger3)))
+print("4. Girilen Değer {} ve Veri Tipi{}".format(deger4,type(deger4)))
+print("5. Girilen Değer {} ve Veri Tipi{}".format(deger5,type(deger5)))
+
+"""
+Not: Bu programda anlamadığım nokta şu. input ile alınan veri her zaman
+string tipinde olacaktır. Kullanıcı tamsayı da girse küsüratlı sayıda girse
+string tipinde olacaktır.
+
+Bu ödevin verildiği noktada gösterilen komutlar ile bu ödevi yapabilmek için
+nasıl olacaktır?
+
+Try Excep ValueError ve while döngüleri ile gelişmiş bir kod yazılabilir.
+
+"""
+
